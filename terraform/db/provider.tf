5f8ff58e4367a33bb60d42d6010c4569eb5013e1
Rene <rene.glavanovits@clouddna.at>
2025-06-19 15:04:02 +0200

    ...

--- terraform/db/provider.tf
+++ terraform/db/provider.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "exoscale/exoscale"
       version = "0.64.1"
     }
+    helm = {
+      source  = "hashicorp/helm" # Helm chart provider
+      version = "2.12.1"         # Specific version
+    }
     kubernetes = {
       source  = "hashicorp/kubernetes" # Kubernetes provider
       version = "2.37.1"               # Specific version
@@ -14,6 +18,12 @@ terraform {
 provider "exoscale" {
   secret = var.exoscale_secret
   key    = var.exoscale_key
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "../kubeconfig"
+  }
 }
 
 provider "kubernetes" {
