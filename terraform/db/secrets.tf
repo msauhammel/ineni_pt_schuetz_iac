44173867e127fecbd0917ecca7e12bb1d7ff2eb6
Rene <rene.glavanovits@clouddna.at>
2025-06-22 21:22:42 +0200

    db

--- terraform/db/secrets.tf
+++ terraform/db/secrets.tf
@@ -8,7 +8,7 @@ resource "kubernetes_secret" "pg_secret" {
     username    = var.pg_admin
     password    = var.pg_password
     #connection_string = "jdbc:${replace(data.exoscale_database_uri.employee-db.uri, "postgres", "postgresql")}"
-    connection_string = "jdbc:postgresql://${data.exoscale_database_uri.employee-db.host}"
+    connection_string = "jdbc:postgresql://${data.exoscale_database_uri.employee-db.host}:${data.exoscale_database_uri.employee-db.port}/defaultdb"
     db_host           = data.exoscale_database_uri.employee-db.host
     db_port           = data.exoscale_database_uri.employee-db.port
   }
